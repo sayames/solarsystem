d36e4c9aa07f785ee20df0800d78c7d601f2aba4
sayames <96351085+sayames@users.noreply.github.com>
2023-07-24 17:20:05 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 // game.js
 
-// Import Three.js and set up the scene, camera, and renderer
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
@@ -8,45 +7,20 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('gameCanvas'), antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-// Add lights to the scene
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
+// Add a spinning cube to the scene
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-directionalLight.position.set(5, 10, 7);
-scene.add(directionalLight);
+camera.position.z = 5;
 
-// Define global variables for the spaceship and asteroids
-let spaceship, asteroids = [];
-
-// Load 3D models and textures
-const loader = new THREE.GLTFLoader();
-loader.load('path/to/spaceship_model.glb', (gltf) => {
-  spaceship = gltf.scene;
-  scene.add(spaceship);
-});
-
-// Function to create asteroids
-function createAsteroid() {
-  // Code to create and position asteroid models goes here
-}
-
-// Function to handle user input and move the spaceship
-function handleUserInput() {
-  // Code to handle user input and update spaceship position and rotation goes here
-}
-
-// Function to update asteroid positions and check for collisions
-function updateAsteroids() {
-  // Code to update asteroid positions and check for collisions with the spaceship goes here
-}
-
-// Main game loop
+// Function to animate the spinning cube
 function animate() {
   requestAnimationFrame(animate);
-  
-  handleUserInput();
-  updateAsteroids();
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
